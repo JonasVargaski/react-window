4d6abd80049fccfc41f26eecb25ffab75991af45
Jonas <jonasvargaski@hotmail.com>
2020-04-15 19:19:35 -0300

    add import Roboto fonts

--- frontend/src/components/ui/Label/styles.js
+++ frontend/src/components/ui/Label/styles.js
@@ -3,7 +3,7 @@ import styled, { css as injectCss } from 'styled-components';
 export const Container = styled.label`
   position: absolute;
   display: table-cell;
-  color: #222;
+  color: #333;
   line-height: 1.4;
   white-space: pre;
   overflow: hidden;
@@ -11,6 +11,7 @@ export const Container = styled.label`
   padding: 0 1px;
   z-index: 2;
   user-select: none;
+  font-weight: 500;
   font-size: ${({ theme }) => theme.fontSizes.label};
   font-family: ${({ theme }) => theme.fonts};
 
